e0f661a90b9ab923fc9122f1fff098b5fe7ffefb
gregzaal <gregzzmail@gmail.com>
2020-03-24 16:29:58 +0200

    Fix lack of error when no files found

--- admin/get_mod_files.php
+++ admin/get_mod_files.php
@@ -5,7 +5,12 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
 if(isset($_POST['slug'])){
     $slug = $_POST['slug'];
 
-    $a = listdir(join_paths($GLOBALS['SYSTEM_ROOT'], "files", "models", $slug));
+    $p = join_paths($GLOBALS['SYSTEM_ROOT'], "files", "models", $slug);
+    if (file_exists($p)){
+        $a = listdir($p);
+    }else{
+        $a = [];
+    }
 
     $encoded = json_encode($a);
     header('Content-type: application/json');
