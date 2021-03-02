bf0026181d4f351615f3918718057618ca18916b
Greg Zaal <gregzzmail@gmail.com>
2021-03-02 13:45:54 +0200

    Update privacy policy to match other Havens for easier maintenance.

--- p/privacy.php
+++ p/privacy.php
@@ -7,56 +7,44 @@ include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
 <div id="page-wrapper">
     <h1>Privacy Info</h1>
 
-    <h2>Patron Names in Footer</h2>
+    <h2>Third Party Cookies</h2>
     <p>
-        The names of all active supporters on Patreon are automatically shown in the footer of every page on this site.
+        We use some third party services (Google Analytics, Revive Adserver) which store cookies on your device that are necessary for using their services, and track data about you for their own purposes. You can read more about what data they store and how they use it on their own Privacy Policies:
     </p>
+    <ul>
+        <li><a href="https://policies.google.com/technologies/partner-sites">Google</a> (site analytics)</li>
+        <li><a href="https://www.revive-adserver.net/privacy/">Revive</a> (banner ads)</li>
+    </ul>
     <p>
-        If you would like your name removed or changed, please contact us: <?php insert_email() ?>
+        If you are not comfortable with this, we recommend you use a trusted open source ad-blocker such as <a href="https://github.com/gorhill/uBlock">uBlock Origin</a>.
     </p>
 
     <h2>IP Logging</h2>
     <p>
-        This server logs your IP address when you download a model (after some minor obfuscation).
+        This server logs a hashed version of your IP address when you download an asset.
     </p>
     <p>
-        This helps us to see which models are downloaded the most by letting us track unique downloads (ignoring multiple downloads of the same model) instead of only total downloads, thus helping us see more accurately what is popular and making decisions about what to make next.
+        This helps us to see which assets are downloaded by the most users instead of only a total download count. Doing this allows us to see what is popular and make decisions about what to shoot next.
     </p>
     <p>
-        If you're not comfortable with this, you can use a VPN to obscure your public IP address.
-    </p>
-
-    <h2>Google Analytics</h2>
-    <p>
-        This website uses <a href="https://analytics.google.com/analytics/web/">Google Analytics</a> to track users who visit each page, which shows us how the site is performing and some basic demographics such as user locations, language, and browser info.
-    </p>
-    <p>
-        Google likely uses this data in some of their own services and systems such as advertisement targeting and behaviour tracking. You can read more about how they use your data <a href="https://policies.google.com/technologies/partner-sites">here</a>.
-    </p>
-    <p>
-        If you are not comfortable with this, you can use an ad-blocker such as <a href="https://github.com/gorhill/uBlock">uBlock Origin</a> to block all tracking by Google Analytics. Note that 3D Model Haven does not display ads, so I'm fully comfortable with and encourage you to use an ad-blocker.
+        If you're not comfortable with this, you can use a VPN to obscure your public IP address. If you wish for us to delete all records downloads from your IP address, you can <?php insert_email("contact us") ?>.
     </p>
 
-    <h2>Disqus</h2>
+    <h2>Patron Names in Footer</h2>
     <p>
-        <a href="https://disqus.com/">Disqus</a> is the third-party commenting system used on this website. They may collect data about you for various purposes similarly to Google Analytics. You can view their Privacy Policy <a href="https://help.disqus.com/terms-and-policies/disqus-privacy-policy">here</a>.
+        The user names of all active supporters on Patreon are automatically shown in the footer of every page on this site.
     </p>
     <p>
-        Again, use an ad-blocker if you want to block these trackers.
-    </p>
-
-    <h2>Cookies</h2>
-    <p>
-        Both Google Analytics and Disqus use cookies to track you and store data. Other than that, cookies are not used anywhere else on this site.
-    </p>
-    <p>
-        Your browser or ad-blocker may allow you to block these cookies.
+        These names are publically accessibly on the Patreon website, but if you would like your name in the footer removed or changed, please contact me either <?php insert_email("by email") ?> or direct message on Patreon.
     </p>
 
     <hr>
 
     <p>
-        If you have any other questions or concerns, please contact us: <?php insert_email() ?>
+        Other than what's mentioned above, we do not store any user data or cookies.
+    </p>
+    <p>
+        If you have any other questions or concerns, please contact me: <?php insert_email() ?>
     </p>
 
 </div>
