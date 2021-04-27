ec8dcb88681af5874797dd9f9400155d21bbcb7a
Greg Zaal <gregzzmail@gmail.com>
2021-04-27 15:32:13 +0200

    Formatting

--- js/functions.js
+++ js/functions.js
@@ -1,24 +1,24 @@
-var click_functions = function(){
+var click_functions = function () {
 
     // Download multiselection
-    $('.download-buttons .sel').click(function() {
-        if ($(this).parent().hasClass("selected")){
+    $('.download-buttons .sel').click(function () {
+        if ($(this).parent().hasClass("selected")) {
             $(this).parent().removeClass("selected");
             $(this).html("<i class=\"material-icons\">check_box_outline_blank</i>");
-        }else{
+        } else {
             $(this).parent().addClass("selected");
             $(this).html("<i class=\"material-icons\">check_box</i>");
         }
     });
     // Download track
-    $('.dl-btn').click(function() {
+    $('.dl-btn').click(function () {
         var id = $(this).attr("id");
         var fhash = $(this).attr("fhash");
-        console.log("Tracking download - id:"+id+", fhash:"+fhash);
-        $.post("dl_click.php", {id: id, fhash: fhash});
+        console.log("Tracking download - id:" + id + ", fhash:" + fhash);
+        $.post("dl_click.php", { id: id, fhash: fhash });
     });
 
-    $('.zip-dl').click(function() {
+    $('.zip-dl').click(function () {
         if (UAParser().browser.name == "Firefox") {
             $('#sw-tab-warning').removeClass('hidden');
         }
@@ -27,12 +27,12 @@ var click_functions = function(){
         var files_parsed = JSON.parse($(this).children('.zip-dl-files').html());
         var files = [];
         let searchParams = new URLSearchParams(window.location.search)
-        files_parsed.forEach(function(f){
+        files_parsed.forEach(function (f) {
             // TEST Load Balancer
-            if (searchParams.has('testdl')){
-                files.push({url: "https://download.polyhaven.com/Models/" + f[0].replace("files/models/", ""), path: f[1]});
-            }else{
-                files.push({url: location.origin + "/" + f[0], path: f[1]});
+            if (searchParams.has('testdl')) {
+                files.push({ url: "https://download.polyhaven.com/Models/" + f[0].replace("files/models/", ""), path: f[1] });
+            } else {
+                files.push({ url: location.origin + "/" + f[0], path: f[1] });
             }
             // END TEST, uncomment line below:
             // files.push({url: location.origin + "/" + f[0], path: f[1]});
--- model/index.php
+++ model/index.php
@@ -1,5 +1,5 @@
 <?php
-include ($_SERVER['DOCUMENT_ROOT'].'/php/functions.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/php/functions.php');
 
 // Parameters
 // Defaults:
@@ -7,15 +7,15 @@ $slug = "none";
 $category = "all";
 
 // Get params (if they were passed)
-if (isset($_GET["m"]) && trim($_GET["m"])){
+if (isset($_GET["m"]) && trim($_GET["m"])) {
     $slug = $_GET["m"];
 }
-if (isset($_GET["c"]) && trim($_GET["c"])){
+if (isset($_GET["c"]) && trim($_GET["c"])) {
     $category = $_GET["c"];
 }
 
 // Redirect if parameters not received
-if (empty($_GET["m"])){
+if (empty($_GET["m"])) {
     header("Location: /models/");
 }
 
@@ -26,21 +26,21 @@ $conn = db_conn_read_only();
 $info = get_item_from_db($slug, $conn);
 
 // Redirect to search if the model is not in the DB.
-if (sizeof($info) <= 1){
-    header("Location: /models/?s=".$slug);
+if (sizeof($info) <= 1) {
+    header("Location: /models/?s=" . $slug);
 }
 
-$canonical = "https://3dmodelhaven.com/model/?m=".$slug;
+$canonical = "https://3dmodelhaven.com/model/?m=" . $slug;
 $t1 = [];
-$t1 ['name'] = $info['name'];
-$t1 ['date_published'] = $info['date_published'];
-$t1 ['author'] = $info['author'];
+$t1['name'] = $info['name'];
+$t1['date_published'] = $info['date_published'];
+$t1['author'] = $info['author'];
 $category_arr = explode(';', $info['categories']);
 $tag_arr = explode(';', $info['tags']);
 $tags = array_merge($category_arr, $tag_arr);
-$t1 ['tags'] = implode(',', array_merge($category_arr, $tag_arr));
+$t1['tags'] = implode(',', array_merge($category_arr, $tag_arr));
 include_start_html($info['name'], $slug, $canonical, $t1);
-include ($_SERVER['DOCUMENT_ROOT'].'/php/html/header.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/php/html/header.php');
 
 
 echo "<div id='item-page'>";
@@ -53,7 +53,7 @@ echo "<a href='/models'>";
 echo "3D Models";
 echo "</a>";
 echo " >";
-if ($category != "all"){
+if ($category != "all") {
     echo " ";
     echo "<a href='/models/?c={$category}'>";
     echo nice_name($category, 'category');
@@ -63,7 +63,7 @@ if ($category != "all"){
 echo "<br><b>{$info['name']}</b></h1>";
 
 $is_published = is_in_the_past($info['date_published']) || $GLOBALS['WORKING_LOCALLY'];
-if ($is_published){
+if ($is_published) {
     echo "<div id='preview-info'>";
 
     echo "<div id='item-preview'>";
@@ -88,36 +88,36 @@ if ($is_published){
     echo "<ul class='item-info-list'>";
 
     echo "<li style='display:flex; align-items: center'>";
-    $author_pic = join_paths($GLOBALS['SYSTEM_ROOT'], "/files/site_images/authors/".$info['author'].".jpg");
-    if (file_exists($author_pic)){
+    $author_pic = join_paths($GLOBALS['SYSTEM_ROOT'], "/files/site_images/authors/" . $info['author'] . ".jpg");
+    if (file_exists($author_pic)) {
         $author_pic = filepath_to_url(get_thumbnail($author_pic, 50, 85));
-        echo "<img class='me-sml' src=\"".$author_pic."\" />";
+        echo "<img class='me-sml' src=\"" . $author_pic . "\" />";
     }
     echo "<span style='flex-grow:100'>";
-    echo "Author:<br/><b><a href=\"/models/?a=".$info['author']."\">";
+    echo "Author:<br/><b><a href=\"/models/?a=" . $info['author'] . "\">";
     echo $info['author'];
     echo "</a>";
     echo "</b>";
     echo "</span>";
     $author_info = get_author_info($info['author'], $conn);
-    if($author_info){
+    if ($author_info) {
         echo "<span style='display:flex; flex-direction:column;'>";
-        if ($author_info['link']){
-            echo "<a href=\"".$author_info['link']."\">";
+        if ($author_info['link']) {
+            echo "<a href=\"" . $author_info['link'] . "\">";
             echo "<i class='material-icons'>link</i>";
             echo "</a>";
         }
-        if ($author_info['email']){
-            echo "<a href=\"mailto:".$author_info['email']."\">";
+        if ($author_info['email']) {
+            echo "<a href=\"mailto:" . $author_info['email'] . "\">";
             echo "<i class='material-icons'>mail_outline</i>";
             echo "</a>";
         }
-        if ($author_info['donate']){
-            if (starts_with($author_info['donate'], "paypal:")){
+        if ($author_info['donate']) {
+            if (starts_with($author_info['donate'], "paypal:")) {
                 $author_info['donate'] = str_replace("paypal:", "", $author_info['donate']);
-                $author_info['donate'] = paypal_email_to_link($author_info['donate'], "3D Model Haven: ".$info['name']);
+                $author_info['donate'] = paypal_email_to_link($author_info['donate'], "3D Model Haven: " . $info['name']);
             }
-            echo "<a href=\"".$author_info['donate']."\">";
+            echo "<a href=\"" . $author_info['donate'] . "\">";
             echo "<i class='material-icons'>favorite_border</i>";
             echo "</a>";
         }
@@ -130,7 +130,7 @@ if ($is_published){
     $category_arr = explode(';', $info['categories']);
     sort($category_arr);
     foreach ($category_arr as $category) {
-        $category_str .= '<a href="/models/?c='.$category.'">'.$category.'</a>, ';
+        $category_str .= '<a href="/models/?c=' . $category . '">' . $category . '</a>, ';
     }
     $category_str = substr($category_str, 0, -2);  // Remove ", " at end
     echo "Categories:<br/><b>{$category_str}";
@@ -141,18 +141,18 @@ if ($is_published){
     $tag_arr = explode(';', $info['tags']);
     sort($tag_arr);
     foreach ($tag_arr as $tag) {
-        $tag_str .= '<a href="/models/?s='.$tag.'">'.$tag.'</a>, ';
+        $tag_str .= '<a href="/models/?s=' . $tag . '">' . $tag . '</a>, ';
     }
     $tag_str = substr($tag_str, 0, -2);  // Remove ", " at end
     echo "Tags:<br/><b>{$tag_str}";
     echo "</b></li>";
 
     echo "<li>";
-    echo "Published:<br/><b>".date("d F Y", strtotime($info['date_published']))."</b><br/>(".time_ago($info['date_published']).")";
+    echo "Published:<br/><b>" . date("d F Y", strtotime($info['date_published'])) . "</b><br/>(" . time_ago($info['date_published']) . ")";
     echo "</li>";
 
     $download_count = get_download_count($info['id'], $conn);
-    $downloads_per_day = round($download_count/((time() - strtotime($info['date_published']))/86400));
+    $downloads_per_day = round($download_count / ((time() - strtotime($info['date_published'])) / 86400));
     echo "<li>";
     echo "Downloads:<br/><b>{$download_count}</b> ({$downloads_per_day} per day)";
     echo "</li>";
@@ -167,19 +167,19 @@ if ($is_published){
     echo "<p class='small'>This model is sponsored by:</p>";
     echo "<ul id='sponsor-list'>";
     $sponsors = get_sponsors($slug, $conn);
-    if ($sponsors){
-        foreach ($sponsors as $s){
+    if ($sponsors) {
+        foreach ($sponsors as $s) {
             echo "<li>";
-            if ($s['url'] != "none" && $s['url'] != ""){
+            if ($s['url'] != "none" && $s['url'] != "") {
                 echo "<a href=\"{$s['url']}\">";
             }
             echo $s['sponsor'];
-            if ($s['url'] != "none" && $s['url'] != ""){
+            if ($s['url'] != "none" && $s['url'] != "") {
                 echo "</a>";
             }
             echo "</li>";
         }
-    }else{
+    } else {
         echo "<li style='font-weight:300'>";
         echo "No one yet :(";
         echo "</li>";
@@ -188,7 +188,7 @@ if ($is_published){
     echo "<p class='small'><a href='https://polyhaven.com/support-us'>Support 3D Model Haven</a><br>to add your name here.</p>";
     echo "</div>";
 
-    if ($info['tmwarn']){
+    if ($info['tmwarn']) {
         echo "<div class='warning-block'><p>";
         echo "<b>Warning:</b><br>";
         echo "This model may contain trademarks or may be based off an existing copyrighted design.<br>While the files below are CC0, use of this asset is at your own risk. ";
@@ -201,7 +201,7 @@ if ($is_published){
     echo "</div>";  // #preview-info
 
 
-    if ($GLOBALS['WORKING_LOCALLY'] && is_in_the_past($info['date_published']) == False){
+    if ($GLOBALS['WORKING_LOCALLY'] && is_in_the_past($info['date_published']) == False) {
         echo "<p style='text-align:center;opacity:0.5;'>(working locally on a yet-to-be-published model)</p>";
     }
 
@@ -209,25 +209,25 @@ if ($is_published){
     echo "<div class='download-buttons'>";
     $sub_dir = join_paths("files", "models", $slug);
     $base_dir = join_paths($GLOBALS['SYSTEM_ROOT'], $sub_dir);
-    if (file_exists($base_dir)){
+    if (file_exists($base_dir)) {
         $downloads = [];
         echo "<div class='main-download-buttons'>";
         echo "<div class='dl-icon'></div>";
-        foreach (listdir($base_dir) as $f){
-            if (basename(pathinfo($f, PATHINFO_FILENAME)) == $slug){
+        foreach (listdir($base_dir) as $f) {
+            if (basename(pathinfo($f, PATHINFO_FILENAME)) == $slug) {
                 $fp = join_paths($base_dir, $f);
                 $ext = pathinfo($fp, PATHINFO_EXTENSION);
                 $fhash = simple_hash(filepath_to_url($fp));
                 $fsize = filesize($fp);
-                $files_list_fp = $fp.".files";
+                $files_list_fp = $fp . ".files";
                 $btn_html = "<div class=\"dl-btn";
                 $files = [];
-                if (file_exists($files_list_fp)){
+                if (file_exists($files_list_fp)) {
                     $files_json = json_decode(file_get_contents($files_list_fp));
                     array_push($files, [join_paths($sub_dir, $f), $f]);
-                    foreach ($files_json as $af){
+                    foreach ($files_json as $af) {
                         $real_fp = join_paths($base_dir, $af);
-                        if (file_exists($real_fp)){
+                        if (file_exists($real_fp)) {
                             array_push($files, [join_paths($sub_dir, $af), $af]);
                             $fsize += filesize($real_fp);
                         }
@@ -235,29 +235,29 @@ if ($is_published){
                     $fhash = simple_hash(filepath_to_url($files_list_fp));
                 }
                 $do_zip = sizeof($files) > 1;
-                if ($do_zip){
+                if ($do_zip) {
                     $btn_html .= " zip-dl";
                 }
-                $btn_html .= "\" id=\"".$info['id']."\" fhash=\"".$fhash."\">";
+                $btn_html .= "\" id=\"" . $info['id'] . "\" fhash=\"" . $fhash . "\">";
                 $btn_html .= format_icon($slug, $fp);
-                $btn_html .= "<p>".$ext."<br><sub>".human_filesize($fsize)."</sub></p>";
-                if ($do_zip){
+                $btn_html .= "<p>" . $ext . "<br><sub>" . human_filesize($fsize) . "</sub></p>";
+                if ($do_zip) {
                     $files = json_encode($files);
                     $btn_html .= "<div class='zip-dl-files hidden' name='{$f}'>{$files}</div>";
                     $btn_html .= "<div class='zip-loading hidden'><div class='loading-animation'><div></div><div></div><div></div><div></div></div></div>";
                 }
                 $always_includes_textures = ['gltf'];
-                if ($do_zip || in_array(strtolower($ext), $always_includes_textures)){
+                if ($do_zip || in_array(strtolower($ext), $always_includes_textures)) {
                     $btn_html .= "<div class='tooltip'>Textures included</div>";
-                }else{
+                } else {
                     $btn_html .= "<div class='tooltip red-text'>No textures included, download them below.</div>";
                 }
                 $btn_html .= "</div>";  // .dl-btn
-                if (!$do_zip){
+                if (!$do_zip) {
                     $dl_url = filepath_to_url($fp);
 
                     // TEST Load Balancer
-                    if (isset($_GET["testdl"])){
+                    if (isset($_GET["testdl"])) {
                         $dl_url = str_replace("/files/models", "https://download.polyhaven.com/Models", $dl_url);
                     }
                     // END TEST
@@ -273,7 +273,7 @@ if ($is_published){
         echo "<div class='fake-table'>";
         fill_file_table($info, $base_dir);
         echo "</div>";  // .fake-table
-    }else{
+    } else {
         echo "<p class='red-text center'><b>";
         echo "Can't find any files! Please ";
         insert_email("let us know");
@@ -283,20 +283,19 @@ if ($is_published){
 
 
     $similar = get_similar($slug, $conn);
-    if ($similar){
+    if ($similar) {
         echo "<h2>";
         echo "Similar Models";
         echo "</h2>";
         echo "<div id='similar-items'>";
         echo "<div id='tex-grid'>";
-        foreach ($similar as $s){
+        foreach ($similar as $s) {
             echo make_grid_item($s);
         }
         echo "</div>";
         echo "</div>";
     }
-
-}else{
+} else {
     echo "<h1 class='coming-soon'>Coming soon :)</h1>";
 }
 
@@ -311,6 +310,6 @@ echo "</div>";  // #item-page
 
 
 <?php
-include ($_SERVER['DOCUMENT_ROOT'].'/php/html/footer.php');
-include ($_SERVER['DOCUMENT_ROOT'].'/php/html/end_html.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/php/html/footer.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/php/html/end_html.php');
 ?>
